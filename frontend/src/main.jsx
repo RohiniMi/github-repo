31433cbefc60e05c4133949d1d3e51183f5e5f00
Rohini Mittal <65945373+RohiniMi@users.noreply.github.com>
2026-05-18 21:36:15 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-
+if (
+  window.matchMedia("(prefers-color-scheme: dark)").matches
+) {
+  document.documentElement.classList.add("dark");
+}
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
